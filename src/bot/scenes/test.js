73293d1c00efad00b2e25a73fa66019e40822dd4
bysinka <63862600+Daria0310@users.noreply.github.com>
2025-01-11 00:59:15 +0500

    git add

--- src/bot/scenes/test.js
+++ src/bot/scenes/test.js
@@ -11,52 +11,7 @@ testScene.enter(async (ctx) => {
     await ctx.reply("Добро пожаловать в Love Bot!")
 })
 
-testScene.command("test", async (ctx) => {
-    await ctx.reply("test")
-})
 
-testScene.command('reply_keyboard', async (ctx) => {    
-    await ctx.reply("Выберите один из вариантов", {
-        reply_markup: {
-            keyboard: [
-                [{ text: 'Вариант 1' }, { text: 'Вариант 2' }],
-                [{ text: 'Вариант 3' }, { text: 'Вариант 4' }],
-                [{ text: 'Вариант 5' }, { text: 'Вариант 6' }],
-            ],
-            resize_keyboard: true,
-        },
-    })
-})
-
-
-
-testScene.command('anket', async (ctx) => {
-    const keyboard = Markup.inlineKeyboard([
-        Markup.button.callback('Лайкнуть', 'like'),
-        Markup.button.callback('Проигнорировать', 'ignore'),
-        Markup.button.callback('Главное меню', 'main_menu'),
-        Markup.button.callback('Перестать искать анкеты', 'stop_searching'),
-    ], { columns: 1 }).reply_markup;
-    await ctx.reply("Выберите один из вариантов", {
-        reply_markup: keyboard
-    })
-})
-
-testScene.action("like", async ctx => {
-    await ctx.reply("Анкета лайкнута")
-})
-
-testScene.action("ignore", async ctx => {
-    await ctx.reply("Анкета проигнорирована")
-})
-
-testScene.action("main_menu", async ctx => {
-    await ctx.reply("Главное меню")
-})
-
-testScene.action("stop_searching", async ctx => {
-    await ctx.reply("Вы перестали искать анкеты")
-})
 
 testScene.on(message('text'), async (ctx) => {
     await ctx.reply(ctx.message.text)
--- src/bot/scenes/wizard.js
+++ src/bot/scenes/wizard.js
@@ -1,27 +1,39 @@
 import { WizardScene } from "telegraf/scenes";
 
-const wizardScene = new WizardScene('wizard', async (ctx) => {
-    ctx.session.name = ctx.message.text;
-    await ctx.reply('Введите ваш возраст');
-    return ctx.wizard.next();
-}, async (ctx) => {
-    if(isNaN(ctx.message.text)) {
-        await ctx.reply('Возраст должен быть числом');
-        ctx.wizard.back();
-        await ctx.wizard.steps[ctx.wizard.cursor](ctx);
-        return;
-    }
-    ctx.session.age = +ctx.message.text;
-    await ctx.reply(`Ваше имя: ${ctx.session.name}, ваш возраст: ${ctx.session.age}`);
-    await ctx.scene.enter('test');
-});
-
+const wizardScene = new WizardScene('createProfileScene', 
+    async (ctx) => {
+        await ctx.reply('Введите ваше имя');
+        return ctx.wizard.next();
+    },
+    async (ctx) => {
+        if(!ctx.session.name) {
+            ctx.session.name = ctx.message.text;
+        }
+        await ctx.reply('Введите ваш возраст');
+        return ctx.wizard.next();
+    }, 
+    async (ctx) => {
+        if(isNaN(ctx.message.text)) {
+            await ctx.reply('Возраст должен быть числом');
+            ctx.wizard.back();
+            await ctx.wizard.steps[ctx.wizard.cursor](ctx);
+            return;
+        }
+        ctx.session.age = +ctx.message.text;
+        await ctx.reply('Укажите ваш пол', {
+            reply_markup: {
+                keyboard: [
+                    [{ text: 'Мужчина' }, { text: 'Женщина' }],
+                ],
+                resize_keyboard: true,
+            },
+        });
+    },
+  
+);
 wizardScene.leave(async (ctx) => {
     await ctx.reply('Вы покинули сцену');
-});
+})  
 
-wizardScene.enter(async (ctx) => {
-    await ctx.reply('Введите ваше имя');
-});
 
 export default wizardScene
